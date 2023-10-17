3a9297e0b71f46ebc57606759f3a4438c46f483f
lenayoo <lenayoo17@gmail.com>
2023-10-17 21:41:30 +0900

    modify structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Nav from './pages/Nav'
 function App() {
 
   return (
-    <>
+    <div className='app'>
      <Nav />
-     <h1>Lena's Book Club</h1>
-     <img src='book-main.jpeg' className='main-img'/>
-    </>
+     <h1>Lena's Library</h1>
+     <div className='main-img'>
+      <img src='book-main.jpeg' className='book-main'/>
+     </div>
+    </div>
   )
 }
 
