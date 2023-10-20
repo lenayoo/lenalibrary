87d0699b99641de3ced9d1102b5260d482836bb3
lenayoo <lenayoo17@gmail.com>
2023-10-21 08:08:38 +0900

    add input handler

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,17 +1,38 @@
-import './styles.css'
+/* eslint-disable no-undef */
+import "./styles.css";
+import { useState } from "react";
 
 function Form() {
+  const [bookquotes, setBookQuotes] = useState({
+    bookInput: "",
+    quotesInput: "",
+  });
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setBookQuotes({ ...bookquotes, [name]: value });
+  };
+  console.log(bookquotes.bookInput, bookquotes.quotesInput);
+
   return (
-    <div className='main-form__div'>
-    <form className='main-form'>
-    <label>Add your book</label>
-    <input />
-    <label>Add quotes</label>
-    <textarea />
-    <button>submit</button>
-    </form>
- </div>
-  )
+    <div className="main-form__div">
+      <form className="main-form">
+        <label>Add your book</label>
+        <input
+          name="bookInput"
+          value={bookquotes.bookInput}
+          onChange={inputHandler}
+        />
+        <label>Add quotes</label>
+        <textarea
+          name="quotesInput"
+          value={bookquotes.quotesInput}
+          onChange={inputHandler}
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,14 +1,16 @@
-import './styles.css'
+import "./styles.css";
 
 function Nav() {
   return (
-    <div className='navbar'>
-        <div className="navbar__logo">LOGO</div>
-        <div className='navebar__menu'><li>Home</li>
+    <div className="navbar">
+      <div className="navbar__logo">LOGO</div>
+      <div className="navebar__menu">
+        <li>Home</li>
         <li>Books and quotes</li>
-        <li>Find myBook</li></div>
+        <li>Find myBook</li>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
