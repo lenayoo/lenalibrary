957e70f8795d0bef95e974ae21ff214731c619c3
lenayoo <lenayoo17@gmail.com>
2023-10-27 13:09:53 +0900

    add navbar design

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 function Nav() {
   return (
     <nav>
-      <ul>
+      <ul className="navbar">
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/booksandquotes">Books and Quotes</Link>
+          <Link to="/findmybook">Find My Book</Link>
         </li>
         <li>
-          <Link to="/findmybook">Find My Book</Link>
+          <Link to="/booksandquotes">Books and Quotes</Link>
         </li>
       </ul>
     </nav>
