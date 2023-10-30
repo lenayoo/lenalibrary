a39332191594fb144c8f1dc0ff343a4ca519cce4
lenayoo <lenayoo17@gmail.com>
2023-10-30 22:15:51 +0900

    try to add api

--- src/pages/FindBook.jsx
+++ src/pages/FindBook.jsx
@@ -1,14 +1,44 @@
 import Nav from "./Nav";
-import "./styles.css";
+import { useEffect, useState } from "react";
 
 function FindBook() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    // Ensure that the Google Books API is loaded
+    if (window.google && window.google.books) {
+      setBooks(google.books.load());
+    }
+  }, []);
+
+  function handleSearch() {
+    if (window.google && window.google.books) {
+      const viewer = new window.google.books.DefaultViewer(
+        document.getElementById("viewerCanvas")
+      );
+
+      viewer.load(`ISBN:${searchTerm}`);
+    }
+  }
+  console.log(books);
   return (
     <div>
       <Nav />
       <form className="findbook__form">
         <label htmlFor="search">Search for the book you are looking for</label>
-        <input />
-        <button>Search</button>
+        <input
+          type="text"
+          placeholder="Enter ISBN or book title"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {/* <button onClick={handleSearch}>Search</button> */}
+        <div
+          id="viewerCanvas"
+          style={{ width: "600px", height: "500px" }}
+        ></div>
+        <div>{books}</div>
       </form>
     </div>
   );
