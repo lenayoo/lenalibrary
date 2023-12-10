2408a159246b58dbea6d0631ca4ea1cf7a4e0c56
lenayoo <lenayoo17@gmail.com>
2023-12-11 00:26:59 +0900

    test

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -23,23 +23,8 @@ function Archive() {
         </div>
         {isOpen && (
           <div className="book-content" onClick={openBookHandler}>
-            <h1>
-              나날들은 지나가고, 세월들은 사라지고, 우리는 기적들 사이를
-              맹목적으로 걸어다닌다.
-            </h1>
-            <p>
-              당신은 확고하게 발을 디디며 앞으로 나아간다. 어디로 향하고 있는지
-              잘 모를 것이다. 안다고 할 수 없을 것이다. 하지만 이제 당신이 깃든
-              풍경은 데자뷔의 속성을 갖는다. 깊숙한 상상 속 어딘가, 언젠가
-              가봤던 곳, 당신은 그것이 빚어지는 것을 인지한다. 당연해. 당신은
-              생각한다.{" "}
-              <u>
-                <strong>바로 이거야.</strong>
-              </u>{" "}
-              글을 쓰고 있는 건 당신이 아니다. 딱히 그렇지 않다 당신이 어둠
-              속에서 빚은 이것, 몇 번이고 실패하고 있을지도 모르겠다고 느꼈던
-              그것이 이제 상냥한 거인처럼 당신을 이끈다.
-            </p>
+            나날들은 지나가고, 세월들은 사라지고, 우리는 기적들 사이를
+            맹목적으로 걸어다닌다.
           </div>
         )}
       </div>
@@ -50,10 +35,22 @@ function Archive() {
         bookContent="図書館と陰の話"
       />
       <Content
+        imgSrc="hesse.png"
+        title="Spring"
+        authorName="Herman Hesse"
+        bookContent="다시 한 번 시작하고 싶다. 방랑하며 열심히 구하고 별들이 있는 곳까지 방황하는 소망들의 어두운 울림이 닿게 하고 싶다. 나는 머뭇거리며 창문을 닫고 불을 켜니 하얗게 빛나는 침대의 베개가 기다리는 것이 보이고, 세계의 주위에 하얗게 빛나는 달과 바깥에 부는 남풍 속에서 은빛 정원 위로 구름의 시가 흘러 가는 것을 안다."
+      />
+      <Content
         imgSrc="dune.png"
         title="Dune"
         authorName="Frank Herbert"
-        bookContent="Fear and desert, fear not"
+        bookContent="I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will permit it to pass over me and through me. And when it has gone past i will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain"
+      />
+      <Content
+        imgSrc="manseeksgod.png"
+        title="누구에게나 신이 필요한 순간이 있다"
+        authorName="Eric Weiner"
+        bookContent="Have you met your God yet?"
       />
       <Content
         imgSrc="manseeksgod.png"
