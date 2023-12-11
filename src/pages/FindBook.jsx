33dad98cd59afe33a221a17e688ef21eca07e060
lenayoo <lenayoo17@gmail.com>
2023-12-11 22:13:13 +0900

    fix google api

--- src/pages/FindBook.jsx
+++ src/pages/FindBook.jsx
@@ -2,43 +2,45 @@ import Nav from "./Nav";
 import { useEffect, useState } from "react";
 
 function FindBook() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [books, setBooks] = useState([]);
+  const [bookTitles, setBookTitles] = useState([]);
 
   useEffect(() => {
-    // Ensure that the Google Books API is loaded
-    if (window.google && window.google.books) {
-      setBooks(google.books.load());
-    }
+    const handleResponse = (response) => {
+      const titles = response.items.map((item) => item.volumeInfo.title);
+      setBookTitles((prevTitles) => [...prevTitles, ...titles]);
+    };
+
+    // Fetch data from the Books API
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.googleapis.com/books/v1/volumes?q=murakami"
+        );
+        const data = await response.json();
+        handleResponse(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  function handleSearch() {
-    if (window.google && window.google.books) {
-      const viewer = new window.google.books.DefaultViewer(
-        document.getElementById("viewerCanvas")
-      );
-
-      viewer.load(`ISBN:${searchTerm}`);
-    }
-  }
-  console.log(books);
   return (
     <div>
       <Nav />
       <form className="findbook__form">
         <label htmlFor="search">Search for the book you are looking for</label>
-        <input
-          type="text"
+        {/* <input
           placeholder="Enter ISBN or book title"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        /> */}
         {/* <button onClick={handleSearch}>Search</button> */}
-        <div
-          id="viewerCanvas"
-          style={{ width: "600px", height: "500px" }}
-        ></div>
-        <div>{books}</div>
+        <h2>Search books</h2>
+        {bookTitles.map((title, index) => (
+          <div key={index}>{title}</div>
+        ))}
       </form>
     </div>
   );
