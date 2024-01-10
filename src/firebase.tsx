ce769bebfd0a840dffcd54c769683679b8c116bb
lenayoo <lenayoo17@gmail.com>
2024-01-10 20:28:41 +0900

    add firebase app

---
+++ src/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBP7TwH4hOw1M18lF98-yGvtrojDJQj18k",
+  authDomain: "lenas-library.firebaseapp.com",
+  projectId: "lenas-library",
+  storageBucket: "lenas-library.appspot.com",
+  messagingSenderId: "186904741660",
+  appId: "1:186904741660:web:5fd76a57becf2264e35cd9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
